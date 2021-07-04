42f562407d8cac4f2c86c9ffab5a18c719be7f50
Eva-Maria <upebo@student.kit.edu>
2021-07-04 19:21:06 +0200

    added JavaDoc to some UsageModel classes

--- bundles/org.palladiosimulator.generator.fluent/src/org/palladiosimulator/generator/fluent/usageModel/api/IUsageModel.java
+++ bundles/org.palladiosimulator.generator.fluent/src/org/palladiosimulator/generator/fluent/usageModel/api/IUsageModel.java
@@ -1,8 +1,8 @@
 package org.palladiosimulator.generator.fluent.usageModel.api;
 
-public interface IUsageModel extends IUsageModelAddition{
+/**
+ * The Interface IUsageModel.
+ */
+public interface IUsageModel extends IUsageModelAddition {
 
-    /* TODO UsageModel hat keinen Namen*
-    IUsageModel withName(String name);
-*/
 }
