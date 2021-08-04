0df87fc6592a640e3800338bd333f6f382a40e57
Yves <10582363+YvesKirschner@users.noreply.github.com>
2021-08-04 15:19:17 +0200

    Update Primitive.java

--- bundles/org.palladiosimulator.generator.fluent_api/src/repositoryStructure/internals/Primitive.java
+++ bundles/org.palladiosimulator.generator.fluent_api/src/repositoryStructure/internals/Primitive.java
@@ -1,10 +1,11 @@
 package repositoryStructure.internals;
-
+// TODO Look where called and adjust it for float and short.
 /**
  * Represents built-in primitive data types available in the repository, i.e.
  * '<em><b>boolean</b></em>', '<em><b>integer</b></em>',
  * '<em><b>string</b></em>', '<em><b>double</b></em>', '<em><b>long</b></em>',
- * '<em><b>char</b></em>', '<em><b>byte</b></em>'.
+ * '<em><b>char</b></em>', '<em><b>byte</b></em>',
+ * '<em><b>float</b></em>', '<em><b>short</b></em>'.
  * 
  * @author Louisa Lambrecht
  */
@@ -36,5 +37,13 @@ public enum Primitive {
 	/**
 	 * primitive data type byte
 	 */
-	BYTE
+	BYTE,
+	/**
+	 * primitive data type float
+	 */
+	FLOAT,
+	/**
+	 * primitive data type short
+	 */
+	SHORT
 }
